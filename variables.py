0eea622f5766f19b06f87b329e13211ddbd072a3
gurimm <gurimunoz@gmail.com>
2023-04-13 16:09:41 -0600

    Inicio del Proyecto

---
+++ variables.py
@@ -0,0 +1,19 @@
+#Definiendo una variable con camelCase
+nombreCompleto = "Lucas Dalto"
+
+#Definiendo una variable con snake_case
+nombre_completo = "Lucas Dalto"
+
+#concatenar con +
+bienvenida ="Hola" + "¿Cómo estás?"
+
+
+#concatenar con f-strings
+bienvenida =f"Hola {nombre} ¿Cómo estás?"
+
+#operadores de pertenencis (in / not in)
+
+print("ola" in bienvenida) #True
+print("ola" not in bienvenida) #False
+
+
